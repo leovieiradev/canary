d3e75e5a1b647fc68d9135cd91b4d4a3f991daad
deadpool <mi.leonardovieira@gmail.com>
2025-09-21 21:16:11 -0300

    up

--- data-otservbr-global/migrations/51.lua
+++
@@ -1,5 +0,0 @@
-function onUpdateDatabase()
-	logger.info("Updating database to version 51 (feat: player death participants)")
-
-	db.query("ALTER TABLE `player_deaths` ADD COLUMN `participants` TEXT NOT NULL;")
-end
